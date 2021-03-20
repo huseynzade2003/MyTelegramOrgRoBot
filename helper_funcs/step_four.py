4ab65f83f76829f5681c447042491c21e708a0a3
huseynzade2003 <73742151+huseynzade2003@users.noreply.github.com>
2021-03-20 05:27:44 +0400

    Update step_four.py

--- helper_funcs/step_four.py
+++ helper_funcs/step_four.py
@@ -1,22 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (c) Shrimadhav U K
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-""" STEP FOUR """
-
 import random
 import requests
 
@@ -30,9 +11,9 @@ def create_new_tg_app(
         app_platform,
         app_desc
 ):
-    # pylint: disable-msg=too-many-arguments
-    """ creates a new my.telegram.org/apps
-    using the provided parameters """
+
+    """ yeni bir my.telegram.org/apps yaradır
+    təqdim olunan parametrlərdən istifadə etmək """
     request_url = "https://my.telegram.org/apps/create"
     custom_header = {
         "Cookie": stel_token
