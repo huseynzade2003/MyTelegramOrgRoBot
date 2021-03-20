d2c2d0c452f07740a48adc26982226990a0e869f
huseynzade2003 <73742151+huseynzade2003@users.noreply.github.com>
2021-03-20 06:04:17 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,17 +1,17 @@
 class Translation(object):
-    START_TEXT = """Salam! Davam etmək üçün Telefon nömrənizi daxil edin
- Məni istifadə etdiyiniz üçün təşəkkür edirəm 😬
+    START_TEXT = """Salam! Mən sizin API məlumatlarınızı daha sürətli və rahat şəkildə almağınız üçün @Mr_HD_20 tərəfindən hazırlanmış botam.
+Davam etmək üçün Telefon nömrənizi daxil edin✍🏻
+Məni istifadə etdiyiniz üçün təşəkkür edirəm😁
 
-/start verib prosesə yenidən başlaya bilərsiniz"""
-    AFTER_RECVD_CODE_TEXT = """Görürəm!
-indi zəhmət olmasa Telegramın sizə göndərdiyi mūvəqqəti Telegram kodunu göndərin!
+Sahibim: @Mr_HD_20"""
+    AFTER_RECVD_CODE_TEXT = """Görürəm! İndi zəhmət olmasa Telegramın sizə göndərdiyi mūvəqqəti Telegram kodunu göndərin!
 
-Bu kod API kimliyini my.telegram.org saytından əldə etmək üçün istifadə olunur.
-ẞəxsi məlumatlarınız heç bir halda 3cü tərəfə ötürülür! GİZLİLİYİNİZ QORUNUR!!!
+Bu kod API məlumatınızı əldə etməyiniz üçün vacibdir.
+ẞəxsi məlumatlarınız heç bir halda 3cü tərəfə ötürülmür! **GİZLİLİYİNİZ QORUNUR!!!**
 
 Məlumatlarınızı yenidən daxil etmək istəyisinizsə /start verin"""
-    BEFORE_SUCC_LOGIN = "kod alındı. Veb səhifəni qırmaq..."
-    ERRED_PAGE = "Xəta başş verdi. Hesabınızın kimliyini əldə etmək alınmadı. \n\n@Mr_HD_20"
-    CANCELLED_MESG = "Əlvida! Xahiş edirəm bot söhbətini yenidən başladın /start"
+    BEFORE_SUCC_LOGIN = "Kod alındı. Göndərilir..."
+    ERRED_PAGE = "Xəta baş verdi. Hesab məlumatlarınızı əldə etmək alınmadı. \n\n@Mr_HD_20"
+    CANCELLED_MESG = "Əlvida! /start verərək botu yenidən başladın"
     IN_VALID_CODE_PVDED = "Xəta❌ Daxil etdiyiniz giriş kodu etibarlı bir Telegram Web Giriş kodu deyil!"
     IN_VALID_PHNO_PVDED = "Xəta❌ Daxil etdiyiniz telefon nömrəsi etibarlı bir telefon nömrəsi deyil!"
